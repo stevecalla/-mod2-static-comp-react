42fea3f1e7a5c5bbf485166f667efd470625b339
Steve <callasteven@gmail.com>
2021-05-25 09:02:14 -0600

    Add footer text and format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footerbar'>
+      Data provided by Marvel. © 2021 Marvel
+    </div>
+  )
+}
+
+export default Footer
