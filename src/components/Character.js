b549d300565152044ffb6f938430dbe83a757144
Steve <callasteven@gmail.com>
2021-05-25 22:28:17 -0600

    Adjust formating for search bcheckbox

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CharacterCards from './CharacterCards';
 import CharacterButton from './CharacterButton.js';
 import './Character.css';
+import magnify from './assets/magnify.png';
+import clearXicon from './assets/clearXicon.png';
 
 const Character = () => {
 
@@ -11,13 +13,15 @@ const Character = () => {
       <div className='character-nav'>
         <h1 className='detail-text'>Character Details</h1>
         <CharacterButton />
-        <div className="arrow-up hidden" id='checkboxArrow'></div>
-          <form className='checkbox-form hidden' id='checkboxMenu'>
+        {/* <div className="arrow-up hidden" id='checkboxArrow'></div> */}
+        <div className="arrow-up" id='checkboxArrow'></div>
+          {/* <form className='checkbox-form hidden' id='checkboxMenu'> */}
+          <form className='checkbox-form' id='checkboxMenu'>
             <div className='search-bar'>
-              <img className='search-icon' loading='lazy' src='./assets/magnify-1.1s-200px.png' alt='Search icon'/>
+              <img className='search-icon' loading='lazy' src={magnify} alt='Search icon'/>
               <input type="text" placeholder="Search All" className="search-input" id="searchInput" spellCheck='true' title='Searchbox'></input>
               <label className='hidden' htmlFor='searchInput' aria-hidden='false'>Search box</label>
-              <img className='clear-icon hidden' loading='lazy' id='clearIcon' src='./assets/add-1.1s-200px.png' alt='Clear icon htmlFor search box'/>
+              <img className='clear-icon hidden' loading='lazy' id='clearIcon' src={clearXicon} alt='Clear icon htmlFor search box'/>
             </div>
             <fieldset className='random-checkbox' id='randomCheckboxWrapper'>
               <legend></legend>
@@ -27,6 +31,37 @@ const Character = () => {
             <fieldset className='character-checkbox'>
               <legend></legend>
               <div id='populateCheckbox'></div>
+              <input type='checkbox' id='randomCheckbox' value='1'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>1</label>
+              <br></br>
+              {/* <br></br> */}
+              <input type='checkbox' id='randomCheckbox' value='2'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>2</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='3'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>3</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='4'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>4</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='5'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>5</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='6'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>6</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='7'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>7</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='8'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>8</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='9'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>9</label>
+              <br></br>
+              <input type='checkbox' id='randomCheckbox' value='10'></input>
+              <label htmlFor='randomCheckbox' id='randomNumberText'>10</label>
+              <br></br>
               {/* <a id='load-more' href="#">Load 10 More</a> */}
               {/* <button onClick={foo}>Perform action</button> */}
               <button>Load 10 More</button>
