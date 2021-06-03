41e97314689671ba3f36f1bc7d5bf25359102694
Steve <callasteven@gmail.com>
2021-06-02 22:56:20 -0600

    Initial checkbox coding

--- src/components/TestRender.js
+++ src/components/TestRender.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 
-const TestRender = (props) => {
-  console.log('a=', props);
-  return (
-    <>
-      <label>
-          <input loading='lazy' type='checkbox' id='checkBox' value=''></input>
-          {props.name}
-        </label>
-        <br></br>
-      {/* <button onClick={(event) => props.testChange(event)}>🗑</button> */}
-    </>
-  )
+class TestRender extends Component {
+  constructor(props) {
+    // console.log('a=', props);
+    super();
+    this.state = {
+      name: props.name,
+      characterClicked: "",
+    }
+    console.log(this.state.characterClicked);
+  }
+
+  handleClick = (event) => {
+    console.log('z=', event);
+    const { value } = event.target;
+    this.setState({
+        characterClicked: value,
+    })
+
+    console.log(this.state.characterClicked);
+}
+
+  render(props) {
+    return (
+      <>
+        <label>
+            <input 
+              loading='lazy' 
+              type='checkbox' 
+              id='checkBox' 
+              value={this.state.name}
+              onClick={this.handleClick}
+             />
+            {this.state.name}
+          </label>
+          <br></br>
+        {/* <button onClick={(event) => props.testChange(event)}>🗑</button> */}
+      </>
+    )
+  }
 }
 
 export default TestRender;
