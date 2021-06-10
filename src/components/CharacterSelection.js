4683b28c3e411f887cd5731a5a7cab4e722ff7e3
Steve <callasteven@gmail.com>
2021-06-10 17:11:44 -0600

    Add search and random checkbox

--- src/components/CharacterSelection.js
+++ src/components/CharacterSelection.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import './CharacterButton.css';
 
+import magnify from './assets/magnify.png';
+import clearXicon from './assets/clearXicon.png';
+
 // const SelectCharacters = ({ handleSubmit, createCheckboxes }) => {
 const CharacterSelection = (props) => {  
   
@@ -14,9 +17,9 @@ const CharacterSelection = (props) => {
     return value ? 'hidden' : '';
   }
 
-  // function hideToggle() {
-  //   return value ? '' : 'hidden';
-  // }
+  function hideToggle() {
+    return value ? '' : 'hidden';
+  }
 
   return (
     <>
@@ -31,6 +34,23 @@ const CharacterSelection = (props) => {
       </button>
       <div className={`arrow-up ${showToggle()}`} id='checkboxArrow'></div>
         <form onSubmit={props.handleSubmit}  className={`checkbox-form ${showToggle()}`} id='checkboxMenu'>
+
+
+        <div className='search-bar'>
+            <img className='search-icon' loading='lazy' src={magnify} alt='Search icon'/>
+            <input type="text" placeholder="Search All" className="search-input" id="searchInput" spellCheck='true' title='Searchbox'></input>
+            <label className='hidden' htmlFor='searchInput' aria-hidden='false'>Search box</label>
+            <img className={`clear-icon ${hideToggle()}`} loading='lazy' id='clearIcon' src={clearXicon} alt='Clear icon htmlFor search box'/>
+          </div>
+          <fieldset className={`random-checkbox ${showToggle()}`} id='randomCheckboxWrapper'>
+            <legend></legend>
+            {/* <input type='checkbox' id='randomCheckbox' value='Get 8 Random Characters' checked={`random-checkbox ${checkedToggle}`}></input> */}
+            {/* <input type='checkbox' id='randomCheckbox' value='Get 8 Random Characters' checked={`random-checkbox ${value ? console.log('y') : console.log('n') }`} onChange={checkedToggle}></input> */}
+            <input type='checkbox' id='randomCheckbox' value='Get 8 Random Characters'></input>
+            <label htmlFor='randomCheckbox' id='randomNumberText'>Get 8 Random Characters</label>
+          </fieldset>
+
+
           <fieldset className='character-checkbox'>
             <legend></legend>
             <div id='populateCheckbox'>{props.createCheckboxes}</div>
